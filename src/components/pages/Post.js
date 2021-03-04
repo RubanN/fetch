7ecd29b7358031438bd6d0121a5d631606f310a6
Ruban8 <vruban573@gmail.com>
2021-03-04 16:24:14 +0530

    fetch

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -9,7 +9,7 @@ export default class Post extends Component {
         this.state = {
              title:'',
              body:'',
-             userId:''
+             userId:""
         }
     
     }
